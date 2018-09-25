61d671304c1cb0d46c694d6d51441979b883fb09
zsmith3 <spike.smith@hotmail.com>
2018-09-25 17:11:16 +0100

    default.js to Platform.urls

--- js/controllers/Platform.js
+++ js/controllers/Platform.js
@@ -1,5 +1,35 @@
 // Platform-specific functions
 var Platform = {
+	urls: {
+		get serverUrl () { return window.localStorage.serverUrl; },
+
+		getPageUrl (page, query) {
+			return page + ".html" + (query ? ("?" + query) : "");
+		},
+
+		getCurrentAddress: function () {
+			return new URLSearchParams(window.location.search).get("address") || "";
+		},
+
+		getCurrentQuery: function () {
+			let params = new URLSearchParams(window.location.search);
+			params.delete("address");
+			return params.toString();
+		},
+
+		getDisplayUrl: function (url) {
+			var query;
+			if (url.indexOf("?") !== -1) {
+				query = new URLSearchParams(url.substr(url.indexOf("?")));
+				url = url.substr(0, url.indexOf("?"));
+			} else query = new URLSearchParams();
+
+			query.set("address", url);
+
+			return "index.html?" + query.toString();
+		}
+	},
+
 	fileTransfer: null,
 
 	// Get the full paths of all files in a directory and subdirectories
