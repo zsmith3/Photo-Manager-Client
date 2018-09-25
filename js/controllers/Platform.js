9305044661c243e216d86263e9cc7357ffd20972
zsmith3 <spike.smith@hotmail.com>
2018-09-25 17:04:36 +0100

    Add functions to Platform.urls

--- js/controllers/Platform.js
+++ js/controllers/Platform.js
@@ -1,5 +1,26 @@
 // Platform-specific versions of functions
 var Platform = {
+	urls: {
+		serverUrl: "/fileserver/",
+
+		getPageUrl: function (page, query) {
+			if (page == "index") page = "";
+			return "/fileserver/" + page + (query ? ("?" + query) : "");
+		},
+
+		getCurrentAddress: function () {
+			return window.location.pathname.substr(11);
+		},
+
+		getCurrentQuery: function () {
+			return window.location.search;
+		},
+
+		getDisplayUrl: function (url) {
+			return "/fileserver/" + url;
+		}
+	},
+
 	getImgSrc: function (object, size) {
 		switch (object.type) {
 		case "file":
