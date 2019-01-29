13695b2be2340d2cbeb41b77d1428f28d86c1ad6
Z Smith <zsmith3.dev@gmail.com>
2019-01-29 18:01:08 +0000

    Add Travis-CI build to Github Pages
    Add build scripts and travis integration, fix various errors

---
+++ scripts/default_build_config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    browser: {
+        server: "https://localhost/fileserver/",
+        outDir: "dist",
+        publicUrl: "/",
+        preScript: false
+    },
+    cordova: {
+        server: "https://localhost/fileserver/",
+        outDir: "cordova/www",
+        publicUrl: "./",
+        preScript: false
+    }
+};
